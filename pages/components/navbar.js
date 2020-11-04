a42c243a8ffbfd5c11706a981289900d2f2299ff
87johnboyle <john.hazukisan@gmail.com>
2020-11-04 14:00:05 +0000

    tweaks

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -1,4 +1,3 @@
-
 import Link from 'next/link';
 import styles from '../styles/navbar.module.css'
 
@@ -12,30 +11,25 @@ export default function NavBar({ children }) {
             
       
 
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <svg width="2em" height="1em" viewBox="0 0 16 16" class="bi bi-people" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1h7.956a.274.274 0 0 0 .014-.002l.008-.002c-.002-.264-.167-1.03-.76-1.72C13.688 10.629 12.718 10 11 10c-1.717 0-2.687.63-3.24 1.276-.593.69-.759 1.457-.76 1.72a1.05 1.05 0 0 0 .022.004zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10c-1.668.02-2.615.64-3.16 1.276C1.163 11.97 1 12.739 1 13h3c0-1.045.323-2.086.92-3zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"/>
-</svg>
-  <button class="navbar-toggler" type="button" 
-  data-toggle="collapse" data-target="#navbarNav" 
-  aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-      <Link  href="/"><a class="navbar-brand"> Home</a></Link>
-      </li>
-      <li class="nav-item">
-      <Link  href="/components/projects"><a class="navbar-brand">Projects</a></Link>
-      </li>
-     
-    </ul>
-    <span class="navbar-text">
-      Code Scramblers Simply from love to code
-    </span>
-  </div>
-</nav>
+ <nav className="nav">
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/components/projects">Projects</Link>
+          </li>
+          <li>
+            <Link href="/"><i className="logo"></i></Link>
+          </li>
+          <li>
+            <Link href="/components/join">Join Us</Link>
+          </li>
+          <li>
+            <Link href="/components/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
 
 
     </div>
